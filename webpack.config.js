1a7be926eb75796051831cdf70ce5e62c0256089
Алексей Князьков <knyazkov_1992@mail.ru>
2021-09-14 17:08:09 +0300

    Докрутил сборку фронтенда.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,40 @@
-const path = require('path');
-const { VueLoaderPlugin } = require('vue-loader')
+const { VueLoaderPlugin } = require('vue-loader');
+const TerserPlugin = require('terser-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const isDev = process.env.NODE_ENV == 'development';
+const isProd = !isDev;
 
 module.exports = {
-    watch: true,
     module: {
         rules: [
-            { test: /\.js$/, use: 'babel-loader' },
+            { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
             { test: /\.vue$/, use: 'vue-loader' },
-            { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
+            { test: /\.css$/, use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+            ]},
         ]
+    },
+    optimization: {
+        minimize: isProd,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            }),
+            new OptimizeCSSAssetsPlugin(),
+        ],
     },
     plugins: [
         new VueLoaderPlugin(),
-    ]
+        new MiniCssExtractPlugin({
+            filename: '../css/app.css',
+        }),
+    ],
 };
